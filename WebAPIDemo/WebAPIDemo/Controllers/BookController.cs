c22edb4350444cfc83923291afdd3d80ddb83039
Preethigavs <preethi.srinivasan@gavstech.com>
2023-09-21 14:02:57 +0530

    include book and book controller

---
+++ WebAPIDemo/WebAPIDemo/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        static List<Book> bookList = new List<Book>();
+        [HttpGet]
+        public List<Book> GetBooks()
+        {
+           
+            for (int i = 0; i < 5; i++)
+            {
+                Book book = new Book();
+                book.BookID = i;
+                book.Title = "Atomic habits" + i;
+                book.Cost = i * 100;
+                book.AuthorName = "Author" + i;
+                bookList.Add(book);
+            }
+            return bookList;
+        }
+        [HttpPost]
+        public int AddBook(Book book)
+        {
+            bookList.Add(book);
+            return 1;
+        }
+
+      
+
+
+
+
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPIDemo.Models
+{
+    public class Book
+    {
+        public int BookID { set; get; }
+        public String Title { set; get; } = String.Empty;
+        public float Cost { set; get; }
+        public String AuthorName { set; get; } = String.Empty;
+
+    }
+}
