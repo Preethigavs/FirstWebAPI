995c4d786977c6bcdbeac01def17a198237fb495
Preethi Srinivasan <preethi.srinivasan@gavstech.com>
2023-09-21 12:07:21 +0530

    web api

---
+++ WebAPIDemo/WebAPIDemo/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+
+        // api/Calculator/Add?x=50&y=50
+       
+        [HttpGet("Calculator/Add")] // if u giving unique name  then give /new name
+        
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+       // api/Calculator/Subtract? x = 500 & y = 50
+       [HttpPost("/Sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        // api/Calculator/Multiply?x=50&y=50
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/Calculator/Divide?x=100&y=50
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpPost("demo/Sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y +100;
+        }
+    }
+}
